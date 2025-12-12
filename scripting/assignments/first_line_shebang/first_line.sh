44b530e80b22a1ce885f51a37859d07c04f1a6fc
sanskar <sanskarsinghss123@gmail.com>
2025-12-12 15:40:30 +0530

    -_-

--- scripting/assignments/first_line_shebang/first_line.sh
+++ scripting/assignments/first_line_shebang/first_line.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/bash
 
-SB="#!/usr/bin/bash"
+cd scripts
 
+SB="#!/usr/bin/bash"
 
 for sf in *.sh; do
 
-    if [ -f "$sf" ]; then
-        echo "Processing file: $sf"
+  if [ -f "$sf" ]; then
+    echo "Processing file: $sf"
 
-        fl=$(head -n 1 "$sf")
+    fl=$(head -n 1 "$sf")
 
-        if [ "$fl" != "$SB" ]; then
-            echo "-> Missing or incorrect shebang. Inserting '$SB'..."
+    if [ "$fl" != "$SB" ]; then
+      echo "-> Missing or incorrect shebang. Inserting '$SB'..."
 
-            sed -i "1i\\$SB" "$sf"
+      sed -i "1i\\$SB" "$sf"
 
-            echo "Successfully inserted the shebang."
-        else
-            echo "Shebang already present. No changes made."
-        fi
+      echo "Successfully inserted the shebang."
+    else
+      echo "Shebang already present. No changes made."
     fi
+  fi
 done
+
