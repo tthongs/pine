1e05c90954fafd10fbfac0f31bf062df3e6f9500
sanskar <sanskarsinghss123@gmail.com>
2025-12-12 14:46:23 +0530

    -_-

---
+++ scripting/assignments/first_line_shebang/first_line.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+SB="#!/usr/bin/bash"
+
+
+for sf in *.sh; do
+
+    if [ -f "$sf" ]; then
+        echo "Processing file: $sf"
+
+        fl=$(head -n 1 "$sf")
+
+        if [ "$fl" != "$SB" ]; then
+            echo "-> Missing or incorrect shebang. Inserting '$SB'..."
+
+            sed -i "1i\\$SB" "$sf"
+
+            echo "Successfully inserted the shebang."
+        else
+            echo "Shebang already present. No changes made."
+        fi
+    fi
+done
