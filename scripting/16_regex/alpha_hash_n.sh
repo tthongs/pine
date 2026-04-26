2433b98268d6af3ed7a2a66f61461eb4c0602871
tthongs <sanskarsinghss123@gmail.com>
2026-04-26 11:23:26 +0530

    refactor(org): reorganize project structure and clean up duplicates

--- scripting/16_regex/alpha_hash_n.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-touch op_alpha_hash
-
-while IFS= read -r line; do
-  if [[ $line =~ ^[a-zA-Z] ]] || [[ $line =~ ^# ]]; then
-    echo "$line" >>op_alpha_hash
-  fi
-done <inp_tt.txt
--- scripting/16_regex/matchrecurring.awk
+++
@@ -1,7 +0,0 @@
-BEGIN{
-str = "this is awkawkawk match cmd."
-m=match(str,"(awk)+")
-print RSTART
-print RLENGTH
-print m
-  }
